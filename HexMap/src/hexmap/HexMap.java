5ae9bc2e3094446a68cb729a3e59cd67e2b1d247
Cormac Walsh <cormacnwalsh@gmail.com>
2019-02-25 16:27:15 +0000

    TEST - Radius changed from 4 to 3

--- HexMap/src/hexmap/HexMap.java
+++ HexMap/src/hexmap/HexMap.java
@@ -3,7 +3,7 @@ package hexmap;
 public class HexMap {
 
 	public static void main(String[] args) {
-		int hexRadius = 4;
+		int hexRadius = 3;
 		Graph hexMap = new Graph(hexRadius);
 		
 		hexMap.printGraph();
