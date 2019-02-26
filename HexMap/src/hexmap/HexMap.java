a3cf2048fbed9095dc88e741fe02445f166f80f8
Cormac <cormacnwalsh@gmail.com>
2019-02-26 21:41:55 +0000

    Added user input for hexRadius

--- HexMap/src/hexmap/HexMap.java
+++ HexMap/src/hexmap/HexMap.java
@@ -5,18 +5,22 @@ import java.util.*;
 public class HexMap {
 
 	public static void main(String[] args) {
-		int hexRadius = 3;
-		Graph hexMap = new Graph(hexRadius);
-
-		hexMap.printGraph();
+//		int hexRadius = 3;
 
 		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter number of hex grid rings: ");
+		int hexRadius = input.nextInt();
 
 		System.out.println("Enter starting hex: ");
 		int start = input.nextInt();
 
 		System.out.println("Enter goal hex: ");
 		int end = input.nextInt();
+
+		Graph hexMap = new Graph(hexRadius);
+
+		hexMap.printGraph();
 
 		hexMap.setPath(start, end);
 
